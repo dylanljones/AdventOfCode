37aeb605183055a53eadb2796f09a8e0059bb51d
dylanljones <dylanljones94@gmail.com>
2023-12-20 20:31:01 +0100

    update solution 2023 day 18

--- 2023/day18/solution.py
+++ 2023/day18/solution.py
@@ -2,38 +2,63 @@
 # Author: Dylan Jones
 # Date:   2023-12-18
 
-import numpy as np
+from itertools import pairwise
+
+import networkx as nx
 
 import aoc
 
-DIRS = {
-    "R": (+1, 0),
-    "L": (-1, 0),
-    "U": (0, +1),
-    "D": (0, -1),
-}
+DIRS = {"R": 1 + 0j, "U": 0 + 1j, "L": -1 + 0j, "D": 0 - 1j}
 ORDER = "RDLU"
 
 
-def find_vertices(data, use_color=False):
-    pos = np.array([0, 0])
-    vertices = [pos.copy()]
-    for dir_, steps, color in map(str.split, data.splitlines(keepends=False)):
+def parse_input(data, use_color=False):
+    graph = nx.Graph()
+    a = 0j
+    for line in data.splitlines():
+        dir_, steps, color = line.split()
         if use_color:
             steps, dir_ = int(color[2:-2], 16), ORDER[int(color[-2])]
         else:
             steps = int(steps)
-        pos += np.array(DIRS[dir_]) * steps
-        vertices.append(pos.copy())
-
-    return np.array(vertices)
+        direction = DIRS[dir_]
+        b = a + steps * direction
+        graph.add_node(a, dout=direction, steps=steps)
+        graph.add_node(b, din=direction)
+        graph.add_edge(a, b)
+        a = b
+    return graph
 
 
 def shoelace(vertices):
-    xs, ys = vertices.T
-    n = ((xs * (np.roll(ys, 1) - np.roll(ys, -1))).sum()) / 2
-    n += sum(abs(np.diff(ys)) + abs(np.diff(xs))) / 2
-    return int(n) + 1
+    return sum((x1 * y2 - y1 * x2) for ((x1, y1), (x2, y2)) in pairwise(vertices)) / 2
+
+
+def get_graph_volume(graph):
+    turn_coords = {(-1j, -1j): 1, (-1j, 1j): 0, (1j, 1j): -1, (1j, -1j): 0}
+
+    turn_total = sum(d["dout"] / d["din"] for (n, d) in graph.nodes(data=True))
+    if turn_total == -4j:
+        clockwise = 1
+    elif turn_total == 4j:
+        clockwise = -1
+    else:
+        raise ValueError("Invalid turn total: {}".format(turn_total))
+
+    point = 0j
+    outer_vertices = [point]
+    for a, b in nx.find_cycle(graph, point):
+        a = graph.nodes[a]
+        b = graph.nodes[b]
+        aturn = a["dout"] / a["din"] * clockwise
+        bturn = b["dout"] / b["din"] * clockwise
+        coords = turn_coords.get((aturn, bturn), 0)
+        direction, steps = a["dout"], a["steps"] + coords
+        point += direction * steps
+        outer_vertices.append(point)
+
+    vol = shoelace([(c.real, c.imag) for c in outer_vertices]) * -clockwise
+    return int(vol)
 
 
 class Solution(aoc.Puzzle):
@@ -41,13 +66,12 @@ class Solution(aoc.Puzzle):
     year = 2023
 
     def solution_1(self, data: str):
-        verts = find_vertices(data)
-        n = shoelace(verts)
-        return n
+        graph = parse_input(data)
+        return get_graph_volume(graph)
 
     def solution_2(self, data: str):
-        verts = find_vertices(data, use_color=True)
-        print(verts)
+        graph = parse_input(data, use_color=True)
+        return get_graph_volume(graph)
 
 
 if __name__ == "__main__":
