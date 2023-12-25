44ae0f2b37c0cdd690055584b4e47d86d41ad00d
dylanljones <dylanljones94@gmail.com>
2023-12-25 15:05:07 +0100

    improve solution 2021 day 1

---
+++ 2021/day01/solution.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+#
+# This code is part of aoc2022.
+#
+# Copyright (c) 2022, Dylan Jones
+
+import aoc
+
+
+def parse_input(data: str) -> list[int]:
+    return [int(x) for x in data.strip().splitlines()]
+
+
+class Solution(aoc.Puzzle):
+    year = 2021
+    day = 1
+
+    def solution_1(self, data):
+        dd = parse_input(data)
+        return sum(dd[i + 1] > dd[i] for i in range(len(dd) - 1))
+
+    def solution_2(self, data):
+        dd = parse_input(data)
+        return sum(sum(dd[i + 1 : i + 4]) > sum(dd[i : i + 3]) for i in range(len(dd)))
+
+
+if __name__ == "__main__":
+    puzzle = Solution()
+    puzzle.run()
