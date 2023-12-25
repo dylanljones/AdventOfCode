6bbda5595ab770789e81e082fb2aa4dcea18736e
dylanljones <dylanljones94@gmail.com>
2023-12-25 15:13:36 +0100

    update header

--- 2021/day07/solution.py
+++ 2021/day07/solution.py
@@ -1,8 +1,6 @@
-# coding: utf-8
-#
-# This code is part of aoc2022.
-#
-# Copyright (c) 2022, Dylan Jones
+# -*- coding: utf-8 -*-
+# Author: Dylan Jones
+# Date:   2023-12-25
 
 import numpy as np
 
