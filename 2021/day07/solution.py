70ca0df6b1e348ea4ab30e61f5aa1c929a63ea82
dylanljones <dylanljones94@gmail.com>
2023-12-25 15:12:36 +0100

    improve solution 2021 day 7

---
+++ 2021/day07/solution.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+#
+# This code is part of aoc2022.
+#
+# Copyright (c) 2022, Dylan Jones
+
+import numpy as np
+
+import aoc
+
+
+def get_cost(x0: int, positions: list[int]) -> int:
+    return sum(abs(x - x0) * (abs(x - x0) + 1) // 2 for x in positions)
+
+
+class Solution(aoc.Puzzle):
+    year = 2021
+    day = 7
+    test_solution_idx_1 = -7
+    test_solution_idx_2 = -3
+
+    def __init__(self):
+        super().__init__(2021, 7)
+        self.load_info()
+
+    def solution_1(self, data: str):
+        positions = list(map(int, data.split(",")))
+        align = int(np.median(positions))
+        return sum(abs(x - align) for x in positions)
+
+    def solution_2(self, data: str):
+        positions = list(map(int, data.split(",")))
+        align = int(np.mean(positions))
+        cost = get_cost(align, positions)
+        direction = -1 if get_cost(align - 1, positions) < cost else +1
+        while get_cost(align + direction, positions) < cost:
+            align += direction
+            cost = get_cost(align, positions)
+        return get_cost(align, positions)
+
+
+if __name__ == "__main__":
+    puzzle = Solution()
+    puzzle.run(test_only=False)
